4de199c6fde48d734752d3a82328174e9f71373d
zhonglianxi <ZLXzlxxs98@163.com>
2019-04-13 22:59:12 +0800

    nettytest

---
+++ netttytest1/netty1/src/main/java/Client1.java
@@ -0,0 +1,84 @@
+import Client1Handler.Client1Handler;
+import io.netty.bootstrap.Bootstrap;
+import io.netty.buffer.Unpooled;
+import io.netty.channel.*;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioSocketChannel;
+
+import java.util.Scanner;
+import java.util.concurrent.TimeUnit;
+
+public class Client1 {
+    //处理请求和处理服务端响应的线程组
+    private EventLoopGroup group=null;
+    //服务启动相关配置信息
+    private Bootstrap bootstrap=null;
+    public Client1(){
+        init();
+    }
+    private void init(){
+        group=new NioEventLoopGroup();
+        bootstrap=new Bootstrap();
+        bootstrap.group(group);
+        bootstrap.channel(NioSocketChannel.class);
+    }
+    public ChannelFuture doRequest(String host, int port, final ChannelHandler... handlers) throws InterruptedException {
+        /**
+         * 客户端的bootstarp只有handler方法，没有childhandler方法
+         * 方法含义等同ServerBootstarp的childHandler方法
+         * 客户端和服务器必须绑定处理器，必须调用childhandler方法
+         */
+        this.bootstrap.handler(new ChannelInitializer<SocketChannel>() {
+            @Override
+            protected void initChannel(SocketChannel socketChannel) throws Exception {
+                socketChannel.pipeline().addLast(handlers);
+            }
+        });
+        //建立连接
+        ChannelFuture future = this.bootstrap.connect(host, port).sync();
+        return future;
+
+    }
+    public void release(){
+        this.group.shutdownGracefully();
+    }
+
+    public static void main(String[] args) {
+        Client1 client1=null;
+        ChannelFuture future=null;
+        try{
+            client1=new Client1();
+            future= client1.doRequest("localhost",55555,new Client1Handler());
+
+            Scanner s=null;
+            while (true){
+                s=new Scanner(System.in);
+                System.out.println("enter message send to server(enter 'exit' for close client)");
+                String line=s.nextLine();
+                if("exit".equals(line)){
+                    //addlistener -增加监听，当条件满足的收，触发监听器
+                    //ChannelFutureLister.Close- 关闭监听器，代表ChannelFuture执行返回后，关闭连接
+                    future.channel().writeAndFlush(Unpooled.copiedBuffer(line.getBytes("utf-8")))
+                            .addListener(ChannelFutureListener.CLOSE);
+                    break;
+                }
+                future.channel().writeAndFlush(Unpooled.copiedBuffer(line.getBytes("utf-8")));
+                TimeUnit.SECONDS.sleep(1);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }finally {
+            if(null!=future){
+                try {
+                    future.channel().closeFuture().sync();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+            if(null!=client1){
+                client1.release();
+            }
+        }
+    }
+}
---
+++ netttytest1/netty1/src/main/java/Server1.java
@@ -0,0 +1,106 @@
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.*;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.sctp.nio.NioSctpServerChannel;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.channel.socket.nio.NioSocketChannel;
+
+
+public class Server1 {
+
+    //监听线程组，监听客户端请求
+    private EventLoopGroup acceptorGroup = null;
+    //处理客户端相关操作线程组，负责处理与客户端的数据通讯
+    private EventLoopGroup clientGroup = null;
+    //服务启动相关配置信息
+    private ServerBootstrap bootstrap=null;
+    public Server1(){
+        init();
+    }
+    private void init(){
+        //构建线程组默认线程组线程数是cpu的核心数
+        acceptorGroup=new NioEventLoopGroup();
+        clientGroup=new NioEventLoopGroup();
+        bootstrap=new ServerBootstrap();
+        //绑定线程组
+        bootstrap.group(acceptorGroup,clientGroup);
+        //设定通讯模式为NIO
+        bootstrap.channel(NioServerSocketChannel.class);
+        //设定缓冲区大小
+        bootstrap.option(ChannelOption.SO_BACKLOG,1024);
+        //SO_SendBUF发送缓冲区，SO_RCVBUF接收缓冲区，SO_KEEPALIVE开启心跳监测（保证连接有效）
+        bootstrap.option(ChannelOption.SO_SNDBUF,16*1024)
+                .option(ChannelOption.SO_RCVBUF,16*1024)
+                .option(ChannelOption.SO_KEEPALIVE,true);
+    }
+
+    /**
+     * 监听处理逻辑
+     * @param port 监听端口
+     * @param acceptorHandlers 处理器，如何处理客户端请求
+     * @return
+     */
+    public ChannelFuture doAccept(int port, final ChannelHandler... acceptorHandlers) throws InterruptedException {
+        /**
+         * childHandler是服务的bootstarp独有的方法，是用于提供处理对象的
+         * 可以一次性增加若干处理逻辑。是类似责任链模式的处理方式
+         * 增加A，B两个逻辑处理，在处理客户端请求数据的时候，根据A————》B顺序处理
+         *
+         * ChannelInitializer- 用于提供处理器的一个模型对象
+         * 其中定义了一个方法，initChannel方法
+         * 方法是用于初始化处理逻辑责任链条的
+         * 可以保证服务端bootstarp只初始化一次处理器，尽量提供处理逻辑的重用。
+         * 避免反复的创建处理器对象，节约资源开销
+         */
+        bootstrap.childHandler(new ChannelInitializer<SocketChannel>() {
+            @Override
+            protected void initChannel(SocketChannel socketChannel) throws Exception {
+                socketChannel.pipeline().addLast(acceptorHandlers);
+            }
+        });
+        //bind方法--绑定坚挺端口的，serverBootStarp可以绑定多个监听端口
+        //sync开始监听逻辑，返回一个ChannelFuture，返回结果代表的是监听成功后的一个对应的未来结果
+        //可以使用ChannelFuture实现后续的服务器和客户端的交互
+        ChannelFuture future=bootstrap.bind(port).sync();
+        return future;
+
+
+    }
+
+    /**
+     * shutdownGracefully-方法是一个安全关闭的方法，可以保证不放弃任何一个已接受的客户端请求
+     */
+    public void release(){
+        this.acceptorGroup.shutdownGracefully();
+        this.clientGroup.shutdownGracefully();
+    }
+
+    public static void main(String[] args) {
+        ChannelFuture future=null;
+        Server1 server1=null;
+        try {
+            server1=new Server1();
+            future=server1.doAccept(55555,new Server1Hander());
+            System.out.println("server started");
+            //关闭连接的
+            future.channel().closeFuture().sync();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }finally {
+            if(null !=future){
+                try {
+                    future.channel().closeFuture().sync();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+            if(null!=server1){
+                server1.release();
+            }
+        }
+    }
+
+
+
+}
